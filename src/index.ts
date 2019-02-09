96a5f962a526c4414717d166d797065cbd0ae695
Ryan <ryan.c.edge@outlook.com>
2019-02-09 03:41:45 -0500

    Minor fixes

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ server.get('/', async (request, reply) => {
 })
 const start = async () => {
     try {
-        await server.listen(5100)
-        console.log('Server running at:', 5100)
+        await server.listen(5000)
+        console.log('Server running at:', 5000)
     } catch (err) {
         console.log(err)
         server.log.error(err)
